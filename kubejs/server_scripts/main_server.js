9c9595b50d7bab61f1bbb5d8cfb7f11bc6e8973a
Gaelmare <2gaelmare@gmail.com>
2026-08-29 01:31:18 -0600

    Initial 1.21 build of EssentialTFC

--- kubejs/server_scripts/main_server.js
+++
@@ -1,6 +0,0 @@
-// priority 0
-// For Panda's Tree Felling to work with TFC trees and axes, we need to remove all logs from the TFC logging tag so the TFC mechanic doesn't proc.
-//ServerEvents.tags('block', event => {
-    // Removes every log from logging tag, so Panda's Tree Felling will work with TFC trees and axes.
-//   event.removeAll('tfc:logs_that_log')
-//})
